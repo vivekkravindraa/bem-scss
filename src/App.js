68c0bf303fca5a114f2d015035e8b0d0f13f9f75
Vivek Ravindra <r.vivek@kloctechnologies.co.in>
2019-10-10 20:27:29 +0530

    BEM - SCSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="accordion">
+        <div class="accordion__item">
+            <h3 class="accordion__title js-title">Real Lorem Ipsum by 37signals</h3>
+            <p class="accordion__copy accordion__copy--open js-copy">By not having the imagination to imagine what the content "might" be, a design consideration is lost. Meaning becomes obfuscated because "it's just text", understandability gets compromised because nobody realized that this text stuff was actually meant to be read. Opportunities get lost because the lorem ipsum garbage that you used instead of real content didn't suggest opportunities. The text then gets made really small, because, it's not meant to be used, we might as well create loads of that lovely white space.</p>
+        </div>
+        
+        <div class="accordion__item">
+            <h3 class="accordion__title js-title">A random title</h3>
+            <p class="accordion__copy js-copy">By not having the imagination to imagine what the content "might" be, a design consideration is lost. Meaning becomes obfuscated because "it's just text", understandability gets compromised because nobody realized that this</p>
+        </div>
+        
+        <div class="accordion__item">
+            <h3 class="accordion__title js-title">A random title</h3>
+            <p class="accordion__copy js-copy">By not having the imagination to imagine what the content "might" be, a design consideration is lost. Meaning becomes obfuscated because "it's just text", understandability gets compromised because nobody realized that this</p>
+        </div>
+        
+        <div class="accordion__item">
+            <h3 class="accordion__title js-title">A random title</h3>
+            <p class="accordion__copy js-copy">By not having the imagination to imagine what the content "might" be, a design consideration is lost. Meaning becomes obfuscated because "it's just text", understandability gets compromised because nobody realized that this</p>
+        </div>
+        
+        <div class="accordion__item">
+            <h3 class="accordion__title js-title">A random title</h3>
+            <p class="accordion__copy js-copy">By not having the imagination to imagine what the content "might" be, a design consideration is lost. Meaning becomes obfuscated because "it's just text", understandability gets compromised because nobody realized that this</p>
+        </div>
     </div>
   );
 }
